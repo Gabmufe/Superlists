56054efc0602a87bc935c1acf4473efc3979c5d7
Gabriel <gabmufe@hotmail.es>
2019-02-14 15:22:30 +0100

    We've clean up after FT runs

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,9 +2,12 @@ from django.test import LiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
 import time
 #import unittest
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -16,10 +19,18 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Gabriel has heard about a cool new online to-do app.
@@ -45,20 +56,17 @@ class NewVisitorTest(LiveServerTestCase):
         # When he hits enter, the page updates, and now the page lists
         # '1: Buy weightlifting straps' as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy weightlifting straps')
+        self.wait_for_row_in_list_table('1: Buy weightlifting straps')
+
         # There is still a text box inviting her to add another item.
         # He enters 'Use straps to train deadlifts'
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use straps to train deadlifts')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.check_for_row_in_list_table('1: Buy weightlifting straps')
-        self.check_for_row_in_list_table('2: Use straps to train deadlifts')
+        self.wait_for_row_in_list_table('1: Buy weightlifting straps')
+        self.wait_for_row_in_list_table('2: Use straps to train deadlifts')
 
         # Gabriel wonder whether the site will remember her list. Then he sees
         # that the site has generated a unique URL for him -- there is some
